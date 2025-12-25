f687c038393efa24b79309cad06d95d4140ae0d1
HongYi <hong.yi@me.com>
2025-12-25 19:56:38 +0800

    Partically done device selection page

---
+++ AudioRopa/AudioRopa/Pages/DeviceSelectionPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AudioRopa.Pages
+{
+    /// <summary>
+    /// DeviceSelectionPage.xaml 的互動邏輯
+    /// </summary>
+    public partial class DeviceSelectionPage : Page
+    {
+        public DeviceSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Device1Button_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Navigate to Device 1 page
+            // Example: this.NavigationService.Navigate(new Uri("Pages/Device1Page.xaml", UriKind.Relative));
+        }
+
+        private void Device2Button_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Navigate to Device 2 page
+            // Example: this.NavigationService.Navigate(new Uri("Pages/Device2Page.xaml", UriKind.Relative));
+        }
+    }
+}
